e7a7ac3f62d4dc934a30e34d91b36f3ec8843fcf
hooks42 <atgie.cedric2@gmail.com>
2025-01-30 20:00:02 +0100

    /ADD Title + animation on Search bar Appear

--- Movie App/ContentView.swift
+++ Movie App/ContentView.swift
@@ -9,10 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var startApp = false
+    @State private var searchBarY: CGFloat = 0.06
+    
     var body: some View {
-        ZStack {
-            AppBackgroundView()
+        GeometryReader { geo in
+            ZStack {
+                AppBackgroundView()
+                TitleView()
+                    .offset(y: geo.size.height * -0.25)
+                if startApp {
+                    SearchBarView()
+                        .offset(y: geo.size.height * self.searchBarY)
+                }
+            }
+            .onTapGesture {
+                UIApplication.shared.endEditing(true)
+            }
+            .onAppear() {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    withAnimation(.easeInOut(duration: 1.5)) {
+                        self.startApp = true
+                    }
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) {
+                    withAnimation(.easeInOut(duration: 1.5)) {
+                        self.searchBarY = 0
+                    }
+                }
+            }
         }
+        .ignoresSafeArea(.keyboard, edges: .bottom)
     }
 }
 
