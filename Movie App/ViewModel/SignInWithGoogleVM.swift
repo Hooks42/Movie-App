658b100de89ea5ca2ae942171b4eb61d3b49d5bf
hooks42 <atgie.cedric2@gmail.com>
2025-02-02 18:30:15 +0100

    /ADD SignInWithGoogle Created (only back)

---
+++ Movie App/ViewModel/SignInWithGoogleVM.swift
@@ -0,0 +1,62 @@
+//
+//  SignInWithGoogleVM.swift
+//  Movie App
+//
+//  Created by Hook on 02/02/2025.
+//
+
+import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
+
+class AuthViewModel: ObservableObject {
+    @Published var isAuthenticated = false
+    @Published var errorMessage: String?
+
+    func signIn() {
+        
+        // MARK: - Check if Firebase is Connected
+        guard let clientID = FirebaseApp.app()?.options.clientID else {
+            errorMessage = "Firebase configuration error"
+            return
+        }
+
+        // MARK: - Configure Google sign in
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+
+        // MARK: - Get the view to create the popUp for SignIn
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let rootViewController = windowScene.windows.first?.rootViewController else {
+            errorMessage = "No root view controller"
+            return
+        }
+
+        // MARK: - Start Google Connexion --> withPresentig = the view in the Popup
+        GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController) { [weak self] result, error in
+            if let error = error {
+                self?.errorMessage = error.localizedDescription
+                return
+            }
+
+        // MARK: - Get the Connexion token to send it to firebase
+            guard let user = result?.user,
+                  let idToken = user.idToken?.tokenString else {
+                self?.errorMessage = "Authentication error"
+                return
+            }
+
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+
+            
+        // MARK: - signIn to firebase using google credentials
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    self?.errorMessage = error.localizedDescription
+                } else {
+                    self?.isAuthenticated = true
+                }
+            }
+        }
+    }
+}
